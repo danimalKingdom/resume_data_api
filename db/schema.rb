c3af3204980efd32c0e4d4125f9411e34baea974
nordgaardgit config --global user.email “jenordgaard@gmail.com <jenordgaard@gmail.com>
2017-03-13 19:59:18 -0700

    made Capstone model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314021301) do
+ActiveRecord::Schema.define(version: 20170314025807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "capstones", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "url"
+    t.string   "screenshot"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
