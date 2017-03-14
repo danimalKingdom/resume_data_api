a72402e87358b9c988cf0baedf52f5e7f252f4cd
jimhantrix <jimtekanyo@gmail.com>
2017-03-13 21:10:46 -0700

    Made education routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	namespace :api do
+
+
   		#students
 		get "/students" => "students#index"
 		get "/students/:id" => "students#show"
 		patch "/students/:id" => "students#edit"
 		delete "/students" => "students#destroy"
 
-
 	    #experiences
 	    post "/experiences" => "experiences#create"
 	    patch "/experiences/:id" => "experiences#update"
 	    delete "/experiences/:id" => "experiences#destroy"
+
+	    #Educations
+	    post "/education" => "education#create"
+	    patch "/education" => "education#update"
+	    delete "/education" => "education#destroy"
 	end
 end
 
