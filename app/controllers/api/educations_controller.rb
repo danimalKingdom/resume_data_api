990c6ce7e60301759020ae6a0d37c9a8713855a2
jimhantrix <jimtekanyo@gmail.com>
2017-03-13 20:25:13 -0700

    update the education schema

--- app/controllers/api/educations_controller.rb
+++ app/controllers/api/educations_controller.rb
@@ -1,4 +1,4 @@
 class Api::EducationsController < ApplicationController
 
-	
+	 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20170314025807) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "educations", force: :cascade do |t|
+    t.string   "start_date"
+    t.string   "end_date"
+    t.string   "degree"
+    t.string   "university_name"
+    t.text     "details"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
